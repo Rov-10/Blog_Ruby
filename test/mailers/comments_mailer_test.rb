52688b3c3769b3102aae466ae134f802be13e1a1
Rov-10 <chipilla8@ukr.net>
2024-01-29 23:10:24 +0200

    rails test errors fixed

--- test/mailers/comments_mailer_test.rb
+++ test/mailers/comments_mailer_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class CommentsMailerTest < ActionMailer::TestCase
   test "submitted" do
     mail = CommentsMailer.submitted comments(:one)
-    assert_equal "New comment", mail.subject
+    assert_equal "New comment!", mail.subject
     assert_equal ["chipilla88@gmail.com"], mail.to
     assert_equal ["from@example.com"], mail.from
   end
